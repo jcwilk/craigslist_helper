d4ef3d51b939ae5b53c3e8c15e5f520be6f057de
John Wilkinson <jcwilk@gmail.com>
2012-06-24 14:19:04 -0700

    Update master

--- craigslist_helper.rb
+++ craigslist_helper.rb
@@ -12,7 +12,7 @@ Near Dwight and Sacramento in Berkeley<br/>
 All items must go ASAP!!!<br/>
 Email me at <a href='mailto:jcwilk@gmail.com'>jcwilk@gmail.com</a> and I will get back to you right away<br/>
 <br/>"+
-    PAGES.map{|p| get_link(p) }.compact.join("<br/>")
+    PAGES.map{|p| get_link(p) }.compact.join("<br/><br/>")
   end
 
   def get_link(url)
