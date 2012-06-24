35c8ba06d0f036a3423752c8762e7fd65b49ea03
John Wilkinson <jcwilk@gmail.com>
2012-06-24 12:35:02 -0700

    Add dalli

--- craigslist_helper.rb
+++ craigslist_helper.rb
@@ -7,7 +7,7 @@ class CraigslistHelper < Sinatra::Base
     "MOVING SALE<br/>
 Near Dwight and Sacramento in Berkeley<br/>
 All items must go ASAP!!!<br/>
-Email me at jcwilk@gmail.com and I will get back to you right away<br/>
+Email me at <a href='mailto:jcwilk@gmail.com'>jcwilk@gmail.com</a> and I will get back to you right away<br/>
 <br/>"+
     PAGES.map{|p| get_link(p) }.compact.join("<br/>")
   end
