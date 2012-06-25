23671fa6183e5a1f8a27fed83a4ed7c2db71a565
John Wilkinson <jcwilk@gmail.com>
2012-06-25 11:17:14 -0700

    Update master

--- craigslist_helper.rb
+++ craigslist_helper.rb
@@ -4,7 +4,6 @@ class CraigslistHelper < Sinatra::Base
     'http://sfbay.craigslist.org/eby/ele/3098633336.html',
     'http://sfbay.craigslist.org/eby/fuo/3098653182.html',
     'http://sfbay.craigslist.org/eby/fuo/3098673151.html',
-    'http://sfbay.craigslist.org/eby/zip/3098690316.html',
     'http://sfbay.craigslist.org/eby/fuo/3098720830.html',
     'http://sfbay.craigslist.org/eby/fuo/3098735744.html',
     'http://sfbay.craigslist.org/eby/bik/3098773389.html',
